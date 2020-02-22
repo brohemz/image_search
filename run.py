58d89bc0b720899c2c4991eeeaa9a2f487c48f14
MonstrousMoon <theonlypinedapple@gmail.com>
2020-02-22 13:47:24 -0800

    commiting

--- run.py
+++ run.py
@@ -2,21 +2,28 @@
 
 from bs4 import BeautifulSoup
 import requests
-import json
+# import json
 from xml.dom import minidom
 import urllib
+import sys
 import os
 
 my_key = r'507ba75534b0005df0e2d15fb2901a29'
 # my_secret = r'0cd20345117cf04b'
 
 
-def setup():
+def setup(arg):
+    ret = {}
+
+    if arg is not None:
+        ret['data_tag'] = arg
+        print(arg)
     dir = os.path.join(os.getcwd(), 'resources')
     if not os.path.exists(dir):
         os.mkdir(dir)
 
 def getListing():
+
     requestURL = "https://api.flickr.com/services/rest"
 
     headers = {
@@ -25,7 +32,7 @@ def getListing():
 
     body = {
         'method': 'flickr.photos.search',
-        'tags': ['dogs'],
+        'tags': ['dogs'] if setup_dict['data_tag'] is None else setup_dict['data_tag'],
         'api_key': my_key
     }
 
@@ -35,28 +42,33 @@ def getListing():
     return xmldoc.getElementsByTagName('photo')
 
 
-setup()
+def main():
+    print(sys.argv)
+    setup_dict = setup(sys.argv[1])
 
-list = getListing()
+    list = getListing()
 
-for x in range(0, 10):
-    cur = list[x].attributes
+    for x in range(0, 10):
+        cur = list[x].attributes
 
+        farm_id = cur['farm'].firstChild.data
+        server_id = cur['server'].firstChild.data
+        pic_id = cur['id'].firstChild.data
+        pic_secret = cur['secret'].firstChild.data
 
-    farm_id = cur['farm'].firstChild.data
-    server_id = cur['server'].firstChild.data
-    pic_id = cur['id'].firstChild.data
-    pic_secret = cur['secret'].firstChild.data
+        # Fix the links, some parsing isn't working on certain photos
+        if farm_id == '0':
+            print("\nfarm/server = 0!!!\n")
+            continue
 
-    # Fix the links, some parsing isn't working on certain photos
-    if farm_id is '0':
-        continue
+        link = f'https://farm{farm_id}.staticflickr.com/{server_id}/{pic_id}_{pic_secret}.jpg'
+        print(link)
 
-    link = f'https://farm{farm_id}.staticflickr.com/{server_id}/{pic_id}_{pic_secret}.jpg'
-    print(link)
+        urllib.request.urlretrieve(link, f'./resources/pic{x}.jpg')
+    #    pic_file = open(f'./resources/pic{x}.jpg', 'wb')
+    #    pic_data = requests.get(link).content
+    #    pic_file.write(pic_data)
+    #    pic_file.close()
 
-    # urllib.request.urlretrieve(link, f'./resources/pic{x}.jpg')
-    pic_file = open(f'./resources/pic{x}.jpg', 'wb')
-    pic_data = requests.get(link).content
-    pic_file.write(pic_data)
-    pic_file.close()
+if __name__ == "__main__":
+    main()
